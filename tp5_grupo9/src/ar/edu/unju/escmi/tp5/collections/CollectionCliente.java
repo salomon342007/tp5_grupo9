9026b30c9a1bbda1f9e197cae8b9239e85102d3a
Colturas <Pachekin8989@gmail.com>
2025-09-28 18:04:14 -0300

    Agrega métodos para precargar y buscar clientes- Fernandez Jorge

--- tp5_grupo9/src/ar/edu/unju/escmi/tp5/collections/CollectionCliente.java
+++ tp5_grupo9/src/ar/edu/unju/escmi/tp5/collections/CollectionCliente.java
@@ -5,5 +5,18 @@ import java.util.List;
 import ar.edu.unju.escmi.tp5.dominio.Cliente;
 
 public class CollectionCliente {
+    public static List<Cliente> clientes = new ArrayList<>();
 
+    public static void agregarCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
+    public static Cliente buscarPorNombre(String nombre) {
+        for (Cliente c : clientes) {
+            if (c.getNombre().equalsIgnoreCase(nombre)) {
+                return c;
+            }
+        }
+        return null;
+    }
 }
