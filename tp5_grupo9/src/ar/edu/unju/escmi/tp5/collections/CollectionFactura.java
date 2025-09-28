ba1baad6580f02380a07a3207f60f06a2004a7de
Colturas <Pachekin8989@gmail.com>
2025-09-28 18:20:18 -0300

    agrego metodo para guardar y buscar facturas- Fernandez Jorge

--- tp5_grupo9/src/ar/edu/unju/escmi/tp5/collections/CollectionFactura.java
+++ tp5_grupo9/src/ar/edu/unju/escmi/tp5/collections/CollectionFactura.java
@@ -5,5 +5,26 @@ import java.util.List;
 import ar.edu.unju.escmi.tp5.dominio.Factura;
 
 public class CollectionFactura {
+    public static List<Factura> facturas = new ArrayList<>();
 
+    public static void agregarFactura(Factura factura) {
+        facturas.add(factura);
+    }
+
+    public static Factura buscarPorNumero(int numero) {
+        for (Factura f : facturas) {
+            if (f.getNumero() == numero) {
+                return f;
+            }
+        }
+        return null;
+    }
+
+    public static double totalVentas() {
+        double total = 0;
+        for (Factura f : facturas) {
+            total += f.getTotal();
+        }
+        return total;
+    }
 }
