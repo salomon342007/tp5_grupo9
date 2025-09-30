a1c64ecd4c54e394a5746994041d2a70263f164a
brunofernandez877 <fernandezcruzbrunobenjamin@gmail.com>
2025-09-30 00:49:56 -0300

    Comenzando CollectionFactura [Fernnadez Bruno]

--- tp5_grupo9/src/ar/edu/unju/escmi/tp5/collections/CollectionFactura.java
+++ tp5_grupo9/src/ar/edu/unju/escmi/tp5/collections/CollectionFactura.java
@@ -5,5 +5,30 @@ import java.util.List;
 import ar.edu.unju.escmi.tp5.dominio.Factura;
 
 public class CollectionFactura {
+    public static List<Factura> facturas = new ArrayList<>();
 
+    public static void agregarFactura(Factura factura) {
+        facturas.add(factura);
+    }
+
+    public static Factura buscarPorNumero(int numero) {
+        for (Factura f : facturas) {
+            if (f.getNumero() == numero) {
+                return f;
+            }
+        }
+        return null;
+    }
+
+    public static List<Factura> listarFacturas() {
+        return facturas;
+    }
+
+    public static double calcularTotalVentas() {
+        double total = 0;
+        for (Factura f : facturas) {
+            total += f.getTotal();
+        }
+        return total;
+    }
 }
