27e1be2643e30d5ce0774e2388d1b596734a0d9b
Colturas <Pachekin8989@gmail.com>
2025-10-01 20:24:27 -0300

    Cliente, cliente mayorista y minorista corregidos- Fernandez Jorge

--- tp5_grupo9/src/ar/edu/unju/escmi/tp5/dominio/Cliente.java
+++ tp5_grupo9/src/ar/edu/unju/escmi/tp5/dominio/Cliente.java
@@ -1,2 +1,36 @@
+package ar.edu.unju.escmi.tp5.dominio;
 
+public abstract class Cliente {
+    protected String nombre;
+    protected String apellido;
+    protected String direccion;
 
+    public Cliente(String nombre, String apellido, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    // IMPLEMENTACIÓN POR DEFECTO: no aplica descuento
+    // Si una subclase necesita un comportamiento distinto, que haga @Override.
+    public double calcularDescuento(double total) {
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellido + " - " + direccion;
+    }
+}
--- tp5_grupo9/src/ar/edu/unju/escmi/tp5/dominio/ClienteMayorista.java
+++ tp5_grupo9/src/ar/edu/unju/escmi/tp5/dominio/ClienteMayorista.java
@@ -1,2 +1,26 @@
+package ar.edu.unju.escmi.tp5.dominio;
 
+public class ClienteMayorista extends Cliente {
+    private String codigoCliente;
 
+    public ClienteMayorista(String nombre, String apellido, String direccion, String codigoCliente) {
+        super(nombre, apellido, direccion);
+        this.codigoCliente = codigoCliente;
+    }
+
+    public String getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    // No aplicamos un descuento adicional global aquí porque en VentaService
+    // ya se ajusta el precio por bulto (mitad del unitario).
+    @Override
+    public double calcularDescuento(double total) {
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "Mayorista: " + super.toString() + " (cod: " + codigoCliente + ")";
+    }
+}
