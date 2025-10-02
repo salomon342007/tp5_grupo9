54bb2fabb3c2f92fe6449e694931d2f5b63f77bc
brunofernandez877 <fernandezcruzbrunobenjamin@gmail.com>
2025-10-02 02:29:01 -0300

    Ordenamientos de los paquetes corregido [Fernnadez Bruno]

--- tp5_grupo9/src/ar/edu/unju/escmi/tp5/dominio/Cliente.java
+++ tp5_grupo9/src/ar/edu/unju/escmi/tp5/dominio/Cliente.java
@@ -1,6 +1,13 @@
 package ar.edu.unju.escmi.tp5.dominio;
 
+import java.util.Scanner;
+
+import ar.edu.unju.escmi.tp5.collections.CollectionFactura;
+
 public abstract class Cliente {
+    
+    private static Scanner scanner = new Scanner(System.in);
+    
     protected String nombre;
     protected String apellido;
     protected String direccion;
@@ -23,10 +30,34 @@ public abstract class Cliente {
         return direccion;
     }
 
-    // IMPLEMENTACIÓN POR DEFECTO: no aplica descuento
-    // Si una subclase necesita un comportamiento distinto, que haga @Override.
     public double calcularDescuento(double total) {
         return total;
+    }
+
+     
+
+    public static void buscarFacturaPorNumero() {
+        System.out.print("Ingrese número de factura a buscar: ");
+        int nro = readInt();
+
+        Factura f = CollectionFactura.buscarFacturaPorNumero(nro);
+        if (f == null) {
+            System.out.println("No se encontró ninguna factura con ese número.");
+        } else {
+            System.out.println("\n=== FACTURA ENCONTRADA ===");
+            System.out.println(f);
+        }
+    }
+
+    private static int readInt() {
+        while (true) {
+            try {
+                String line = scanner.nextLine().trim();
+                return Integer.parseInt(line);
+            } catch (Exception e) {
+                System.out.print("Entrada inválida. Ingrese un número entero: ");
+            }
+        }
     }
 
     @Override
--- tp5_grupo9/src/ar/edu/unju/escmi/tp5/dominio/Factura.java
+++ tp5_grupo9/src/ar/edu/unju/escmi/tp5/dominio/Factura.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Factura {
+    
     private static int contador = 1;
 
     private int numero;
